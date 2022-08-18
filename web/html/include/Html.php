6218357b9da2ac795be803be1cea2853396ee66d
Björn Mattsson <bjorn@sunet.se>
2022-08-18 15:57:00 +0200

    Fixed bug if no expand or table-sort was used

--- web/html/include/Html.php
+++ web/html/include/Html.php
@@ -7,6 +7,7 @@ Class HTML {
 		$this->startTimer = time();
 		$this->loggedIn = false;
 		$this->tableToSort = array();
+		$this->showDownload = true;
 		switch ($DS) {
 			case 'seamless' :
 				$this->DS = '/DS/seamless-access';
@@ -113,7 +114,7 @@ Class HTML {
     };
   </script><?php }
 		printf('  <!-- jQuery first, then Popper.js, then Bootstrap JS -->%s  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>%s  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>%s  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>%s', "\n", "\n", "\n", "\n");
-		if (isset($this->tableToSort[0]) || isset($collapseIcons[0]) ) {
+		if (isset($this->tableToSort[0]) || isset($collapseIcons[0]) || $this->showDownload ) {
 			if (isset($this->tableToSort[0]))
 				# Add JS script to be able to use later
 				printf('  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>%s', "\n");
@@ -145,14 +146,14 @@ Class HTML {
 				print "    });\n";
 
 			}
-		}?>
+		?>
     // Add the following code if you want the name of the file appear on select
     $(".custom-file-input").on("change", function() {
       //var fileName = $(this).val().split("\\").pop();
       var fileName = $(this).val().split("\\\\").pop();
       $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     });
-  </script>
+	<?php print "  </script>\n"; } ?>
 </body>
 </html>
 <?php
