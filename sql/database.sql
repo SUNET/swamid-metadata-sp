9b4729348f3065bfeaf693bfe5172dfc59e72eaf
Björn Mattsson <bjorn@sunet.se>
2022-09-08 15:27:39 +0200

    Updated sql-syntax

--- sql/database.sql
+++ sql/database.sql
@@ -1,125 +1,124 @@
-CREATE TABLE Entities (
-	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`publishedId` INT UNSIGNED NOT NULL,
-	`entityID` VARCHAR(256), 
-	`registrationInstant` VARCHAR(256),
-	`isIdP` TINYINT UNSIGNED,
-	`isSP` TINYINT UNSIGNED,
-	`publishIn` TINYINT UNSIGNED,
-	`status` TINYINT UNSIGNED,
-	`ALlevel` TINYINT UNSIGNED,
-	`lastUpdated` DATETIME,
-	`lastValidated` DATETIME,
-	`validationOutput` TEXT,
-	`warnings` TEXT,
-	`errors` TEXT,
-	`errorsNB` TEXT,
-	`xml` TEXT);
+CREATE TABLE `Entities` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`publishedId` int(10) unsigned NOT NULL DEFAULT 0,
+	`entityID` varchar(256) DEFAULT NULL,
+	`registrationInstant` varchar(256) DEFAULT NULL,
+	`isIdP` tinyint(3) unsigned DEFAULT NULL,
+	`isSP` tinyint(3) unsigned DEFAULT NULL,
+	`publishIn` tinyint(3) unsigned DEFAULT NULL,
+	`status` tinyint(3) unsigned DEFAULT NULL,
+	`ALlevel` tinyint(3) unsigned DEFAULT NULL,
+	`lastUpdated` datetime DEFAULT NULL,
+	`lastValidated` datetime DEFAULT NULL,
+	`validationOutput` text DEFAULT NULL,
+	`warnings` text DEFAULT NULL,
+	`errors` text DEFAULT NULL,
+	`errorsNB` text DEFAULT NULL,
+	`xml` text DEFAULT NULL);
 
-CREATE TABLE EntityAttributes (
-	`entity_id` INT UNSIGNED,
-	`type` VARCHAR(30),
-	`attribute` VARCHAR(256));
+CREATE TABLE `EntityAttributes` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`type` varchar(30) DEFAULT NULL,
+	`attribute` varchar(256) DEFAULT NULL);
 
-CREATE TABLE Scopes (
-	`entity_id` INT UNSIGNED,
-	`scope` VARCHAR(256),
-	`regexp` TINYINT UNSIGNED);
+CREATE TABLE `Scopes` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`scope` varchar(256) DEFAULT NULL,
+	`regexp` tinyint(3) unsigned DEFAULT NULL);
 
-CREATE TABLE Mdui (
-	`entity_id` INT UNSIGNED,
-	`type` ENUM('SPSSO', 'IDPSSO', 'IDPDisco'),
-	`lang` CHAR(2),
-	`height` SMALLINT,
-	`width` SMALLINT,
-	`element` VARCHAR(25),
-	`data` TEXT);
+CREATE TABLE `Mdui` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`type` enum('SPSSO', 'IDPSSO', 'IDPDisco') DEFAULT NULL,
+	`lang` char(2) DEFAULT NULL,
+	`height` smallint(6) DEFAULT NULL,
+	`width` smallint(6) DEFAULT NULL,
+	`element` varchar(25) DEFAULT NULL,
+	`data` text DEFAULT NULL);
 
-CREATE TABLE KeyInfo (
-	`entity_id` INT UNSIGNED,
-	`type` ENUM('SPSSO', 'IDPSSO', 'AttributeAuthority'),
-	`use` ENUM('both', 'signing', 'encryption'),
-	`order` TINYINT UNSIGNED,
-	`name` VARCHAR(256),
-	`notValidAfter` DATETIME,
-	`subject` VARCHAR(256),
-	`issuer` VARCHAR(256),
-	`bits` SMALLINT UNSIGNED,
-	`key_type` VARCHAR(256),
-	`serialNumber` VARCHAR(44));
+CREATE TABLE `KeyInfo` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`type` enum('SPSSO', 'IDPSSO', 'AttributeAuthority') DEFAULT NULL,
+	`use` enum('both', 'signing', 'encryption') DEFAULT NULL,
+	`order` tinyint(3) unsigned DEFAULT NULL,
+	`name` varchar(256) DEFAULT NULL,
+	`notValidAfter` datetime DEFAULT NULL,
+	`subject` varchar(256) DEFAULT NULL,
+	`issuer` varchar(256) DEFAULT NULL,
+	`bits` smallint(5) unsigned DEFAULT NULL,
+	`key_type` varchar(256) DEFAULT NULL,
+	`serialNumber` varchar(44) DEFAULT NULL);
 
-CREATE TABLE AttributeConsumingService (
-	`entity_id` INT UNSIGNED,
-	`Service_index` SMALLINT UNSIGNED,
-	`isDefault` TINYINT UNSIGNED);
+CREATE TABLE `AttributeConsumingService` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`Service_index` smallint(5) unsigned DEFAULT NULL,
+	`isDefault` tinyint(3) unsigned DEFAULT NULL);
 
-CREATE TABLE AttributeConsumingService_Service (
-	`entity_id` INT UNSIGNED,
-	`Service_index` SMALLINT UNSIGNED,
-	`element` VARCHAR(20),
-	`lang` CHAR(2),
-	`data` TEXT);
+CREATE TABLE `AttributeConsumingService_Service` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`Service_index` smallint(5) unsigned DEFAULT NULL,
+	`element` varchar(20) DEFAULT NULL,
+	`lang` char(2) DEFAULT NULL,
+	`data` text DEFAULT NULL);
 
-CREATE TABLE AttributeConsumingService_RequestedAttribute (
-	`entity_id` INT UNSIGNED,
-	`Service_index` SMALLINT UNSIGNED,
-	`FriendlyName` VARCHAR(256),
-	`Name` VARCHAR(256),
-	`NameFormat` VARCHAR(256),
-	`isRequired` TINYINT UNSIGNED);
+CREATE TABLE `AttributeConsumingService_RequestedAttribute` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`Service_index` smallint(5) unsigned DEFAULT NULL,
+	`FriendlyName` varchar(256) DEFAULT NULL,
+	`Name` varchar(256) DEFAULT NULL,
+	`NameFormat` varchar(256) DEFAULT NULL,
+	`isRequired` tinyint(3) unsigned DEFAULT NULL);
 
-CREATE TABLE Organization (
-	`entity_id` INT UNSIGNED,
-	`lang` CHAR(10),
-	`element` VARCHAR(25),
-	`data` TEXT);
+CREATE TABLE `Organization` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`lang` char(10) DEFAULT NULL,
+	`element` varchar(25) DEFAULT NULL,
+	`data` text DEFAULT NULL);
 
-CREATE TABLE ContactPerson (
-	`entity_id` INT UNSIGNED,
-	`contactType` ENUM('technical', 'support', 'administrative', 'billing', 'other'),
-	`extensions` VARCHAR(256),
-	`company` VARCHAR(256),
-	`givenName` VARCHAR(256),
-	`surName` VARCHAR(256),
-	`emailAddress` VARCHAR(256),
-	`telephoneNumber` VARCHAR(256),
-	`subcontactType` VARCHAR(256));
+CREATE TABLE `ContactPerson` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`contactType` enum('technical', 'support', 'administrative', 'billing', 'other') DEFAULT NULL,
+	`extensions` varchar(256) DEFAULT NULL,
+	`company` varchar(256) DEFAULT NULL,
+	`givenName` varchar(256) DEFAULT NULL,
+	`surName` varchar(256) DEFAULT NULL,
+	`emailAddress` varchar(256) DEFAULT NULL,
+	`telephoneNumber` varchar(256) DEFAULT NULL,
+	`subcontactType` varchar(256) DEFAULT NULL);
 
-CREATE TABLE EntityURLs (
-	`entity_id` INT UNSIGNED,
-	`URL` TEXT,
-	`type` VARCHAR(20),
-	UNIQUE(entity_id, type)
-);
+CREATE TABLE `EntityURLs` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`URL` text DEFAULT NULL,
+	`type` varchar(20) DEFAULT NULL,
+	UNIQUE KEY `entity_id_type` (`entity_id`,`type`));
 
-CREATE TABLE URLs (
-	`URL` TEXT,
-	`type` TINYINT UNSIGNED,
-	`status` TINYINT UNSIGNED,
-	`cocov1Status` TINYINT UNSIGNED,
-	`lastSeen` DATETIME,
-	`lastValidated` DATETIME,
-	`validationOutput` TEXT);
+CREATE TABLE `URLs` (
+	`URL` text DEFAULT NULL,
+	`type` tinyint(3) unsigned DEFAULT NULL,
+	`status` tinyint(3) unsigned DEFAULT NULL,
+	`cocov1Status` tinyint(3) unsigned DEFAULT NULL,
+	`lastSeen` datetime DEFAULT NULL,
+	`lastValidated` datetime DEFAULT NULL,
+	`validationOutput` text DEFAULT NULL);
 
-CREATE TABLE Users (
-	`entity_id` INT UNSIGNED,
-	`userID` TEXT,
-	`email` TEXT,
-	UNIQUE(entity_id, userID));
+CREATE TABLE `Users` (
+	`entity_id` int(10) unsigned DEFAULT NULL,
+	`userID` text DEFAULT NULL,
+	`email` text DEFAULT NULL,
+	UNIQUE KEY `entity_id_userID` (`entity_id`,`userID`) USING HASH);
 
-CREATE TABLE TestResults (
-	`entityID` VARCHAR(256),
-	`test` VARCHAR(20),
-	`time` DATETIME,
-	`result` VARCHAR(70),
-	UNIQUE(entityID, test));
+CREATE TABLE `TestResults` (
+	`entityID` varchar(256) DEFAULT NULL,
+	`test` varchar(20) DEFAULT NULL,
+	`time` datetime DEFAULT NULL,
+	`result` varchar(70) DEFAULT NULL,
+	UNIQUE KEY `entityID_test` (`entityID`,`test`));
 
-CREATE TABLE EntitiesStatus (
-	`date` DATETIME,
-	`ErrorsTotal` INT UNSIGNED,
-	`ErrorsSPs` INT UNSIGNED,
-	`ErrorsIdPs` INT UNSIGNED,
-	`NrOfEntites` INT UNSIGNED,
-	`NrOfSPs` INT UNSIGNED,
-	`NrOfIdPs` INT UNSIGNED,
-	`Changed` INT UNSIGNED);
+CREATE TABLE `EntitiesStatus` (
+	`date` datetime DEFAULT NULL,
+	`ErrorsTotal` int(10) unsigned DEFAULT NULL,
+	`ErrorsSPs` int(10) unsigned DEFAULT NULL,
+	`ErrorsIdPs` int(10) unsigned DEFAULT NULL,
+	`NrOfEntites` int(10) unsigned DEFAULT NULL,
+	`NrOfSPs` int(10) unsigned DEFAULT NULL,
+	`NrOfIdPs` int(10) unsigned DEFAULT NULL,
+	`Changed` int(10) unsigned DEFAULT NULL);
