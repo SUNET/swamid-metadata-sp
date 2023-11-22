672cf24d51d1a19785e9fd4387868686b15c121a
Björn Mattsson <bjorn@sunet.se>
2023-11-22 11:06:43 +0100

    Removed unused script

--- web/scripts/updateFeed.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include "/var/www/metadata/include/Metadata.php";
-	$metadata = new Metadata($argv[1],'Prod');
-	$metadata->updateFeed($argv[2]);
-#	if ($metadata->getResult() <> "Updated in db")
-#		printf ("Import -> %s\n" ,$metadata->getResult());
-?>
