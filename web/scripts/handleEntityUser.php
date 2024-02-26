5c58e039ee736976530d75fb0f72e9011e4e5c5a
Björn Mattsson <bjorn@sunet.se>
2024-02-26 16:00:45 +0100

    Added script to update EntityUsers

---
+++ web/scripts/handleEntityUser.php
@@ -0,0 +1,108 @@
+<?php
+/* Cleanup Database and remove
+    * SoftDeleted entities lastUpdated < 3 months
+    * PublishedPending entities lastUpdated < 3 months
+    * Shadow entities lastUpdated < 4 months (Should be 3 month + 2-3 days)
+*/
+// file deepcode ignore FileInclusion:
+include __DIR__ . '/../html/config.php'; # NOSONAR
+// file deepcode ignore FileInclusion:
+#include __DIR__ . '/../html/include/Metadata.php'; # NOSONAR
+
+const BIND_STATUS = ':Status';
+const BIND_REMOVEDATE = ':RemoveDate';
+
+try {
+  $db = new PDO("mysql:host=$dbServername;dbname=$dbName", $dbUsername, $dbPassword);
+  // set the PDO error mode to exception
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Error Connecting DB";
+}
+
+
+switch ($argc) {
+  case 3 :
+    # UserId as 1:st param
+    # entityID as 2:nd
+    $user = getUser($argv[1]);
+    addAccess($user, $argv[2]);
+    break;
+  case 2 :
+    # UserId as 1:st param
+    $user = getUser($argv[1]);
+    listAccess($user);
+    break;
+  default :
+    # No param list users. Case 1:
+    listUsers();
+    break;
+    
+}
+
+function listUsers() {
+  global $db;
+  $usersHandler = $db->prepare('SELECT `userID`, `fullName` FROM Users ORDER BY `userID`');
+  $usersHandler->execute();
+  while ($user = $usersHandler->fetch(PDO::FETCH_ASSOC)) {
+    printf("%s -> %s\n", $user['userID'], $user['fullName']);
+  }
+  printf("For a list of entities connected to a user :\n   %s <username>\n", __FILE__);
+}
+
+function getUser($userID) {
+  global $db;
+  $usersHandler = $db->prepare('SELECT `id`, `userID`, `fullName` FROM Users WHERE `userID` = :UserID');
+  $usersHandler->execute(array('UserID'=> $userID));
+  if ($user = $usersHandler->fetch(PDO::FETCH_ASSOC)) {
+    return $user;
+  } else {
+    printf ("User %s is missing!\n", $userID);
+    exit;
+  }
+}
+
+function listAccess(&$user) {
+  global $db;
+  printf ("User %s has access to : \n", $user['userID']);
+  $entitiesAccessHandler = $db->prepare("SELECT `entityID`
+      FROM EntityUser, Entities
+      WHERE EntityUser.`entity_id` = Entities.`id`
+        AND EntityUser.`user_id` = :UsersId
+        AND `status` = 1
+      ORDER BY `entityID`, `status`");
+  $entitiesAccessHandler->execute(array('UsersId'=> $user['id']));
+  while ($entity = $entitiesAccessHandler->fetch(PDO::FETCH_ASSOC)) {
+    printf("%s\n", $entity['entityID']);
+  }
+  printf("To add access to more entities :\n   %s %s <entityID>\n", __FILE__, $user['userID']);
+}
+
+function addAccess(&$user, $entityID) {
+  global $db, $argv;
+  $entitiesHandler = $db->prepare("SELECT `id`
+    FROM Entities
+    WHERE `entityID` = :EntityID
+      AND status = 1");
+  $entitiesAccessHandler = $db->prepare("SELECT *
+    FROM EntityUser
+    WHERE `entity_id` = :EntityId
+      AND `user_id` = :UsersId");
+  $entitiesAddAccessHandler = $db->prepare("INSERT
+    INTO EntityUser
+    SET `entity_id` = :EntityId, `user_id` = :UsersId, `approvedBy` = 'Admin', `lastChanged` = NOW()");
+
+  
+  $entitiesHandler->execute(array('EntityID' => $entityID));
+  if ($entity = $entitiesHandler->fetch(PDO::FETCH_ASSOC)) {
+    $entitiesAccessHandler->execute(array('UsersId' => $user['id'], 'EntityId' => $entity['id']));
+    if ($entitiesAccessHandler->fetch()) {
+      printf ("User %s (%s) already had access to %s.\n", $user['userID'], $user['fullName'], $entityID);
+    } else {
+      $entitiesAddAccessHandler->execute(array('UsersId' => $user['id'], 'EntityId' => $entity['id']));
+      printf ("Added %s (%s) to %s.\n", $user['userID'], $user['fullName'], $entityID);
+    }
+  } else {
+    printf ("Entity with entityID %s is missing!\n", $entityID);
+  }
+}
