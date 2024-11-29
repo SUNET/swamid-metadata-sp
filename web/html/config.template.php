a76ac74fa3045c1f281bf97a5dcd85cf3804eec2
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2024-11-29 13:42:11 +1300

    fix: config.template.php: add trailing commas
    To avoid syntax errors when uncommenting subsequent directives.

--- web/html/config.template.php
+++ web/html/config.template.php
@@ -3,7 +3,7 @@ $db = array(
   'name'        => 'metadata',      # Name of Database
   'servername'  => 'mariadb',       # Name of DB server
   'username'    => 'metdata_admin', # Username for DB
-  'password'    => 'adminpwd'       # Password for DB NOSONAR
+  'password'    => 'adminpwd',       # Password for DB NOSONAR
 
   # optional parameter
   
@@ -11,7 +11,7 @@ $db = array(
   # The file path to the SSL certificate authority.
   # Activates PDO::MYSQL_ATTR_SSL_CA in options.
   ###
-  # 'caPath' => '/etc/ssl/CA.pem'
+  # 'caPath' => '/etc/ssl/CA.pem',
 );
 
 $smtp = array(
@@ -28,13 +28,13 @@ $smtp = array(
   ###
   #'sasl'        => array(
   #  'user'      => 'SASL User',           #
-  #  'password'  => 'SASL Password'        #
+  #  'password'  => 'SASL Password',       #
   #),
 
   ###
   # If bcc is set, a copy of outgoing mail will be sent to this address
   ###
-  # 'bcc'         => 'admin@host.se'        #
+  # 'bcc'         => 'admin@host.se',      #
 );
 $mode = 'Lab';
 $baseURL = 'https://metadata.host.se/';
