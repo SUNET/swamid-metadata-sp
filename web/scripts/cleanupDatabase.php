d27fde783cd4666f89b2d75e3e4df68c713b9560
Björn Mattsson <bjorn@sunet.se>
2025-04-28 05:14:15 +0200

    Remove inactive users.

--- web/scripts/cleanupDatabase.php
+++ web/scripts/cleanupDatabase.php
@@ -18,13 +18,15 @@ $flagDates = $config->getDb()->query('SELECT
   NOW() - INTERVAL 3 MONTH AS `removeDate`,
   NOW() - INTERVAL 4 MONTH AS `removeDateShadow`,
   NOW() - INTERVAL 13 WEEK AS `removePending`,
-  NOW() - INTERVAL 9 WEEK AS `removeDraft`', PDO::FETCH_ASSOC);
+  NOW() - INTERVAL 9 WEEK AS `removeDraft`,
+  NOW() - INTERVAL 6 MONTH AS `removeUser`', PDO::FETCH_ASSOC);
 foreach ($flagDates as $dates) {
   # Need to use foreach to fetch row. $flagDates is a PDOStatement
   $removeDate = $dates['removeDate'];
   $removeDateShadow = $dates['removeDateShadow'];
   $removePending = $dates['removePending'];
   $removeDraft = $dates['removeDraft'];
+  $removeUser = $dates['removeUser'];
 }
 $flagDates->closeCursor();
 
@@ -34,6 +36,13 @@ $entitiesUpdatedHandler->bindValue(BIND_REMOVEDATE, $removeDate);
 
 $entitiesValidatedHandler = $config->getDb()->prepare(
   'SELECT id, `entityID`, `lastValidated` FROM Entities WHERE `status` = :Status AND `lastValidated` < :RemoveDate;');
+
+$usersHandler = $config->getDb()->prepare('SELECT `id`, `userID`, `email`, `fullName`, `lastSeen`, COUNT(entity_id) AS count
+    FROM `Users`
+    LEFT JOIN `EntityUser` ON `Users`.`id` = `user_id`
+    WHERE `lastSeen` < :LastSeen OR `lastSeen` IS NULL
+    GROUP BY id;');
+$removeUserHandler = $config->getDb()->prepare('DELETE FROM `Users` WHERE `id` = :Id;');
 
 # Remove SoftDeleted entities
 printf ("SoftDeleted Entities before %s\n", $removeDate);
@@ -90,5 +99,15 @@ while ($entity = $entitiesValidatedHandler->fetch(PDO::FETCH_ASSOC)) {
   $metadata->removeEntity();
   printf (" -> Pending %s %s\n", $entity['entityID'], $entity['lastValidated']);
 }
-
 $entitiesValidatedHandler->closeCursor();
+
+# Remove Old users
+printf ("Users whith no Entities and lastSeen before %s\n", $removeUser);
+$usersHandler->execute(array('LastSeen' => $removeUser));
+while ($user = $usersHandler->fetch(PDO::FETCH_ASSOC)) {
+  if ($user['count'] == 0) {
+    printf('Removing %s (%s) %s%s',
+      $user['email'], $user['fullName'], $user['lastSeen'], "\n");
+    $removeUserHandler->execute(array('Id' => $user['id']));
+  }
+}
