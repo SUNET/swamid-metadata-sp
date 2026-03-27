7e873c077e2d44f8e7a68f76af25a15ffe7905cb
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2026-03-27 17:21:09 +1300

    new: refactor norEduPerson attrs into AttributeDefsSWAMID

--- web/html/src/AttributeDefs.php
+++ web/html/src/AttributeDefs.php
@@ -148,15 +148,6 @@ class AttributeDefs {
     'urn:oid:0.9.2342.19200300.100.1.3' => array(
       'desc' => 'mail', 'standard' => true
     ),
-    'urn:oid:1.3.6.1.4.1.2428.90.1.6' => array(
-      'desc' => 'norEduOrgAcronym', 'standard' => true
-    ),
-    'urn:oid:1.3.6.1.4.1.2428.90.1.10' => array(
-      'desc' => 'norEduPersonLegalName', 'standard' => true
-    ),
-    'urn:oid:1.3.6.1.4.1.2428.90.1.5' => array(
-      'desc' => 'norEduPersonNIN', 'standard' => true
-    ),
     'urn:oid:2.5.4.10' => array(
       'desc' => 'o', 'standard' => true
     ),
---
+++ web/html/src/AttributeDefsSWAMID.php
@@ -0,0 +1,28 @@
+<?php
+namespace metadata;
+
+/**
+ * Class to hold (and allow extending) attribute definitions
+ */
+class AttributeDefsSWAMID extends AttributeDefs {
+  /**
+   * FRIENDLY_NAMES
+   *
+   */
+  const FRIENDLY_NAMES_SWAMID = array(
+    'urn:oid:1.3.6.1.4.1.2428.90.1.6' => array(
+      'desc' => 'norEduOrgAcronym', 'standard' => true
+    ),
+    'urn:oid:1.3.6.1.4.1.2428.90.1.10' => array(
+      'desc' => 'norEduPersonLegalName', 'standard' => true
+    ),
+    'urn:oid:1.3.6.1.4.1.2428.90.1.5' => array(
+      'desc' => 'norEduPersonNIN', 'standard' => true
+    ),
+  );
+
+  public function getAttributeFriendlyNames() {
+    return array_merge(self::FRIENDLY_NAMES, self::FRIENDLY_NAMES_SWAMID);
+  }
+}
+
