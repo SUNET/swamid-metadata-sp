e43590843698de8a9fc4b9b6ef53c08421b78137
Vlad Mencl <vladimir.mencl@reannz.co.nz>
2026-03-31 18:58:20 +1300

    nfc: AttributeDefsTuakiri: sort list, remove TODO note

--- web/html/src/AttributeDefsTuakiri.php
+++ web/html/src/AttributeDefsTuakiri.php
@@ -34,8 +34,6 @@ class AttributeDefsTuakiri extends AttributeDefs {
     'urn:oid:1.3.6.1.4.1.5923.1.1.1.5' => array(
       'desc' => 'eduPersonPrimaryAffiliation', 'standard' => true // accept in Tuakiri
     ),
-
-    // TODO: HIDE UNWANTED SAML attrs
   );
 
   const FRIENDLY_NAMES_EXCLUDE = array(
@@ -47,15 +45,15 @@ class AttributeDefsTuakiri extends AttributeDefs {
     'urn:mace:dir:attribute-def:givenName' => false,
     'urn:mace:dir:attribute-def:mail' => false,
     'urn:mace:dir:attribute-def:sn' => false,
-    'urn:oid:1.2.840.113549.1.9.1.1' => false, // Wrong email
     'urn:oid:0.9.2342.19200300.100.1.10' => false, // manager
-    'urn:oid:2.16.840.1.113730.3.1.4' => false, // employeeType
-    'urn:oid:1.3.6.1.4.1.5923.1.1.1.13' => false, // eduPersonUniqueId
     'urn:oid:0.9.2342.19200300.100.1.1' => false, // uid
-    'urn:oid:1.2.752.29.4.13' => false, // personalIdentityNumber
     'urn:oid:0.9.2342.19200300.100.1.43' => false, // co
+    'urn:oid:1.2.752.29.4.13' => false, // personalIdentityNumber
+    'urn:oid:1.2.840.113549.1.9.1.1' => false, // Wrong email
     'urn:oid:1.3.6.1.4.1.25178.1.2.3' => false, // schacDateOfBirth
+    'urn:oid:1.3.6.1.4.1.5923.1.1.1.13' => false, // eduPersonUniqueId
     'urn:oid:2.16.840.1.113730.3.1.13' => false, // mailLocalAddress
+    'urn:oid:2.16.840.1.113730.3.1.4' => false, // employeeType
     'urn:oid:2.5.4.6' => false, // c
   );
 
